0ed6ad5680ae6c42655bfb3210a4f790b93adf28
andrewshuba <andrew.shuba2@gmail.com>
2020-12-18 13:35:35 +0200

    - fix preview formatting

--- templates/job/content.php
+++ templates/job/content.php
@@ -7,7 +7,7 @@ if ( ! empty( $jb_job_info['job_id'] ) ) {
 
 	<div class="jb-job-content">
 		<div class="jb-job-content-separator"><?php _e( 'Description', 'jobboardwp' ) ?></div>
-		<div class="jb-job-content-section"><?php echo get_the_content( null, false, $job_id ) ?></div>
+		<div class="jb-job-content-section"><?php echo apply_filters( 'the_content', get_the_content( null, false, $job_id ) ); ?></div>
 	</div>
 
 <?php }
