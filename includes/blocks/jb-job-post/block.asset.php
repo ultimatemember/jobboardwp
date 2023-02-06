3604bb6e8009f8d1fcf91e06b7f7c0bd635a17f6
ashubawork <andrew.shuba2@gmail.com>
2023-02-06 15:51:50 +0200

    - wpcs

--- includes/blocks/jb-job-post/block.asset.php
+++ includes/blocks/jb-job-post/block.asset.php
@@ -1,10 +1,10 @@
 <?php return array(
-		'dependencies' => array(
-			'wp-blocks',
-			'wp-element',
-			'wp-components',
-			'wp-server-side-render',
-			'wp-polyfill',
-		),
-		'version'      => '1',
-	);
+	'dependencies' => array(
+		'wp-blocks',
+		'wp-element',
+		'wp-components',
+		'wp-server-side-render',
+		'wp-polyfill',
+	),
+	'version'      => '1',
+);
