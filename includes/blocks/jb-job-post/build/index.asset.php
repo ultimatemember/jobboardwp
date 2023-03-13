19387722b0e3e2a447adbf5eb173595be707c12c
ashubawork <andrew.shuba2@gmail.com>
2023-03-13 10:47:40 +0200

    - job post, job category list blocks

---
+++ includes/blocks/jb-job-post/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-server-side-render'), 'version' => '3b6875e8a1c554434702');
---
+++ includes/blocks/jb-jobs-categories-list/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-server-side-render'), 'version' => '4ec50c51be43819c9059');
