74dd141316310f022a8f4007e5b711cffd2027d4
Mykyta Synelnikov <nsinelnikov.work@gmail.com>
2023-05-24 15:22:54 +0300

    - fixed salary range validation;

--- includes/ajax/class-jobs.php
+++ includes/ajax/class-jobs.php
@@ -1296,7 +1296,7 @@ if ( ! class_exists( 'jb\ajax\Jobs' ) ) {
 						} else {
 							if ( ! is_numeric( $_POST['data']['jb-salary-min-amount'] ) ) {
 								$errors['wrong'][] = 'jb-salary-min-amount';
-							} elseif ( 0 !== absint( $_POST['data']['jb-salary-min-amount'] ) && absint( $_POST['data']['jb-salary-min-amount'] ) >= absint( $_POST['data']['jb-salary-max-amount'] ) ) {
+							} elseif ( 0 !== absint( $_POST['data']['jb-salary-max-amount'] ) && absint( $_POST['data']['jb-salary-min-amount'] ) >= absint( $_POST['data']['jb-salary-max-amount'] ) ) {
 								$errors['wrong'][] = 'jb-salary-min-amount';
 							}
 
