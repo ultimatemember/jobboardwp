c88834ea0c70971df022c46d61f2936433872321
Mykyta Synelnikov <nsinelnikov.work@gmail.com>
2025-05-21 17:04:33 +0300

    Update plugin license to GPLv3
    Standardized the licensing across plugins by updating license and license URI to GPLv3 in plugin headers and readme files. Ensures compliance and clarity regarding licensing terms.

--- jobboardwp.php
+++ jobboardwp.php
@@ -5,6 +5,8 @@
  * Description: Add a modern job board to your website. Display job listings and allow employers to submit and manage jobs all from the front-end
  * Version: 1.3.1
  * Author: JobBoardWP
+ * License: GPLv3
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.txt
  * Text Domain: jobboardwp
  * Domain Path: /languages
  *
